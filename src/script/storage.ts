bc86b5722f769c904902041287f1b89bedb5d802
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-03-31 16:45:23 +0530

    feature:add file to store value

---
+++ src/script/storage.ts
@@ -0,0 +1,28 @@
+import { Post } from './api.ts';
+
+export const getLocalPosts = (): Post[] => {
+    return JSON.parse(localStorage.getItem('posts') || '[]');
+};
+
+export const storePostsLocally = (posts: Post[]) => {
+    const storedPosts = getLocalPosts();
+    const newPosts = posts.filter(
+        (p) => !storedPosts.some((sp) => sp.id === p.id)
+    );
+    localStorage.setItem(
+        'posts',
+        JSON.stringify([...storedPosts, ...newPosts])
+    );
+};
+
+export const updateLocalPost = (updatedPost: Post) => {
+    const posts = getLocalPosts().map((p) =>
+        p.id === updatedPost.id ? updatedPost : p
+    );
+    localStorage.setItem('posts', JSON.stringify(posts));
+};
+
+export const deleteLocalPost = (id: number) => {
+    const posts = getLocalPosts().filter((p) => p.id !== id);
+    localStorage.setItem('posts', JSON.stringify(posts));
+};
